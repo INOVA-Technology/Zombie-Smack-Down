4890e7f254d8d4080a080f4a9174d33da7721637
Addisonbean <addisonbean@gmail.com>
2013-04-27 13:01:33 -0500

    Initial Commit

---
+++ game.rb
@@ -0,0 +1,403 @@
+#!/usr/bin/env ruby
+
+module Stuff
+
+  class Game
+  	attr_accessor :prefs, :r
+    require 'yaml'
+    
+    def initialize
+      prefs_file = File.open 'prefs.yaml', 'r'
+      @prefs = YAML.load prefs_file.read
+      prefs_file.close
+      @default = { xp: 10, kills: 0, health: 20}
+      @r = 0;
+      @prefs[:xp] += @prefs[:rank] * 5
+      @combos = ["kick punch", "punch punch kick", "elbow fist knee fist knee body slam", "heal fury", "trip stomp", "knee punch face slap", "kick kick kick kick kick kick kick kick kick kick kick kick kick kick kick", "chase punch of fire", "addison kick of cold hard music", "ultimate destruction kick punch", "chuck norris stomp of mayhem", "coolest combo ever"]
+    end
+
+    def give_xp amount
+      @prefs[:xp] += amount
+      @prefs[:xp] = 0 if @prefs[:xp] < 0
+    end	
+
+    def die
+      death = [" \e[31mYour dead\e[39m ", " \e[31mToo bad\e[39m ", " \e[31mThe zombie ate ur brainz\e[39m ", " \e[31mSo sad\e[39m ", " \e[31mGame over looser\e[39m ", " \e[31mYou\e[39m must really fail at life "].sample
+      introPhrase = [" You have \e[31mkilled\e[39m ", " You have viciously slapped ", "\e[31m You have beaten the crud out of\e[39m "].sample
+      endPhrase = [" zombies ", " \e[36minnocent\e[39m zombies ", " \e[31mvicious\e[39m zombies ", " \e[31mruthless\e[39m zombies ", " \e[31mwalkers\e[39m "].sample
+
+      prefs_file = File.open 'prefs.yaml', 'w'
+      prefs_file.puts @default.to_yaml + ":totalKills: " + @prefs[:totalKills].to_s + "\n:rank: " + @prefs[:rank].to_s
+      prefs_file.close
+
+      puts death + "(enter to continue)"
+      gets.chomp
+      puts introPhrase + @prefs[:kills].to_s + endPhrase + "(enter to continue)"
+      gets.chomp
+      exit
+    end
+
+    def win
+      puts "You win!!"
+      prefs_file = File.open 'prefs.yaml', 'w'
+      prefs_file.puts @default.to_yaml + ":totalKills: " + @prefs[:totalKills].to_s + "\n:rank: " + @prefs[:rank].to_s
+      prefs_file.close
+    end
+
+    def not_enough_xp
+      puts
+      puts "Not enough xp..."
+      @r = 0
+    end
+
+    def punch
+      @r = Random::rand(4..7)
+    end
+
+    def kick
+      @r = Random::rand(3..8)
+    end
+
+    def combo com
+      case com
+      when "kick punch"
+        if @prefs[:xp] >= 2
+          @r = Random::rand(3..9)
+          self.give_xp -2
+        else
+          self.not_enough_xp
+        end
+      when "punch punch kick"
+        if @prefs[:xp] >= 4 
+          @r = Random::rand(2..12)
+          self.give_xp -4
+        else
+          self.not_enough_xp
+        end
+      when "elbow fist knee fist knee body slam" 
+        if @prefs[:xp] >= 6
+          @r = Random::rand(3..18)
+          self.give_xp -6
+        else
+          self.not_enough_xp
+        end
+      when "trip stomp" 
+        if @prefs[:xp] >= 3
+          @r = Random::rand(4..10)
+          self.give_xp -3
+        else
+          self.not_enough_xp
+        end
+      when "knee punch face slap" 
+        if @prefs[:xp] >= 4
+          @r = Random::rand(6..12)
+          self.give_xp -4
+        else
+          self.not_enough_xp
+        end
+      when "kick kick kick kick kick kick kick kick kick kick kick kick kick kick kick" 
+        if @prefs[:xp] >= 7
+          @r = Random::rand(9..17)
+          self.give_xp -7
+        else
+          self.not_enough_xp
+        end
+      when "chase punch of fire" 
+        if @prefs[:xp] >= 20
+          @r = Random::rand(20..40)
+          self.give_xp -20
+        else
+          self.not_enough_xp
+        end
+      when "addison kick of cold hard music" 
+        if @prefs[:xp] >= 20
+          @r = Random::rand(20..40)
+          self.give_xp -20
+        else
+          self.not_enough_xp
+        end
+      when "ultimate destruction kick punch" 
+        if @prefs[:xp] >= 30
+          @r = Random::rand(1..50)
+          self.give_xp -30
+        else
+          self.not_enough_xp
+        end
+      when "chuck norris stomp of mayhem" 
+        if @prefs[:xp] >= 1000
+          @r = Random::rand(1..2000000)
+          self.give_xp -1000
+        else
+          self.not_enough_xp
+        end
+      when "coolest combo ever" 
+        if @prefs[:xp] >= 15
+          @r = Random::rand(10..25)
+          self.give_xp -15
+        else
+          self.not_enough_xp
+        end
+      when "heal fury"
+        if @prefs[:xp] >= 5
+          @r = Random::rand(4..10)
+          self.damage(Random::rand(2..4) * -1)
+          self.give_xp -5
+        else
+          self.not_enough_xp
+        end
+      end
+    end
+
+    def attack enemy, weapon = "punch"
+      pass = 1
+      phrase = [" You \e[31msmacked down\e[39m the ", " You hit the ", " Whose your daddy ", " You just \e[31mpowned\e[39m the "].sample
+      while pass != 0
+      	pass = 0
+	      case weapon
+	      when "punch"
+	        self.punch
+	      when "kick"
+	      	self.kick
+	      when "combo"
+	      	puts "Which combo?"
+	      	c = gets.chomp.downcase
+	      	if @combos.include? c
+            self.combo c
+		      else
+  	      	puts "Invalid combo..."
+            @r = 0
+	      	end
+	      end
+        
+	    end
+      enemy.damage @r
+      
+      puts
+      puts phrase + enemy.name + " -" + @r.to_s
+      puts
+    end
+
+    def damage amount
+      @prefs[:health] -= amount
+      self.die if @prefs[:health] <= 0
+    end	
+
+    def info
+      puts "\e[35m"
+      @prefs.each{ |key, value|
+        puts key.to_s + ": " + value.to_s
+      }
+      print "\e[39m"
+    end
+
+    def pwn
+      puts "\e[31mKO! \e[39m"
+      puts
+      @prefs[:kills] += 1
+      @prefs[:totalKills] += 1
+      self.rankup if @prefs[:totalKills] % 15 == 0
+    end
+
+    def rankup
+      @prefs[:rank] += 1
+      give_xp 10
+      if (@combos[@prefs[:rank] - 1]) 
+        puts "\e[35mNew combo unlocked!"
+        i = 0
+        @combos.each { |c|
+          break if i == @prefs[:rank]
+          puts c
+          i += 1
+        }
+        print "\e[39m"
+      else
+        puts "\e[35mYour doing pretty good!\e[39m"
+      end
+    end
+
+    def comboList
+      if (@combos[@prefs[:rank] - 1]) 
+        puts "\e[35mCombos:"
+        i = 0
+        @combos.each { |c|
+          break if i == @prefs[:rank]
+          puts c
+          i += 1
+        }
+        print "\e[39m"
+      else
+        puts "\e[35mYour doing pretty good!\e[39m"
+      end
+    end
+
+    def quit
+      puts "Wanna save yo game? yes or no"
+      save = gets.chomp.downcase
+        if save == "yes"
+          prefs_file = File.open 'prefs.yaml', 'w'
+          prefs_file.puts @prefs.to_yaml
+          prefs_file.close
+          exit
+        else
+        	exit
+        end
+    end
+
+    def heal
+      puts "\e[36mHow much health do u want? (1 xp for 1 health)"
+      howMuch = gets.chomp.to_i
+      if howMuch == 0
+        puts "Nothing given"
+      elsif howMuch <= @prefs[:xp] 
+        howMuch *= -1
+	      self.damage howMuch
+        self.give_xp howMuch
+	      puts "you have been healed +" + (howMuch * -1).to_s
+	      puts "\e[39m"
+	    else
+	    	puts "NOT ENOUGH XP!!! >:D"
+			end
+    end
+
+    def rankup
+      give_xp 10
+      puts "Newest combo: " + @combos[@prefs[:rank] - 1] || "Your doing pretty good!"
+    end
+
+    def taunt
+      taunt = ["\e[31mHEY ZOMBIE! UR FACE!\e[39m", "\e[31mDIRT BAG!\e[39m", "\e[31mUR MOM!\e[39m", "\e[31mPOOP FACE\e[39m", "\e[31mGET OWNED BUDDY BOY!\e[39m", "\e[31m:p\e[39m", "\e[31mEAT MY FIST!\e[39m", "\e[31mbe nice\e[39m"].sample
+      
+      if @prefs[:xp] >= 2
+
+      r = Random::rand(-10..10)
+      self.give_xp r
+      puts taunt + " " + r.to_s
+      
+      else
+        puts "\e[31mNOT ENOUGH XP >:p\e[39m"
+      
+      end
+
+    end
+
+  # end of Game class
+  end
+
+  ############### ZOMBIEZ!! ###############
+
+  class Zombie
+    
+    attr_accessor :is_alive
+
+    def initialize hero
+      @is_alive = true;
+      @hero = hero
+      self.setXpPainHealth
+    end
+
+    def setXpPainHealth
+      @xp = 2
+      @pain = [4, 6]
+      @health = 10
+      @name = "Zombie"
+      @phrases = ["hit ur face", "punched the heck out of you", "beat the heck out of you", "bruised ur face"].sample
+    end
+    
+    def name
+      @name
+    end
+
+    def attack
+      if @is_alive
+        r = Random::rand(@pain[0]..@pain[1])
+        @hero.damage r
+      
+        puts self.name + ' ' + @phrases + " -" + r.to_s
+        puts
+      end
+    end
+
+    def die 
+      @hero.give_xp @xp
+      @is_alive = false
+      @hero.pwn
+    end
+
+    def damage amount
+      @health -= amount
+      self.die if @health <= 0
+    end
+
+    def info
+      puts "\e[35m"
+      puts self.name + " health: " + @health.to_s
+      print "\e[39m"
+
+    end
+  # end Zombie class
+  end
+
+  ### MORE ZOMBIEZ! ###
+
+  class BigZombie < Zombie
+    def setXpPainHealth
+      @xp = 4
+      @pain = [6, 8]
+      @health = 15
+      @name = "Big Zombie"
+      @phrases = ["hit ur face", "punched the heck out of you", "beat the heck out of you", "bruised ur face"].sample
+    end
+  end
+
+  class DaddyZombie < Zombie
+    def setXpPainHealth
+      @xp = 10
+      @pain = [3, 12]
+      @health = 20
+      @name = "Daddy Zombie"
+      @phrases = ["IS your daddy", "punched the heck out of you", "beat the heck out of you", "ain't your mom"].sample
+    end
+  end
+
+  class GunZombie < Zombie
+    def setXpPainHealth
+      @xp = 15
+      @pain = [2, 15]
+      @health = 20
+      @name = "Gun Zombie"
+      @phrases = ["shot yo face", "shot the heck out of you", "beat the heck out of you", "made you eat bullets"].sample
+    end
+  end
+
+  class NinjaZombie < Zombie
+    def setXpPainHealth
+      @xp = 20
+      @pain = [7, 20]
+      @health = 20
+      @name = "Ninja Zombie"
+      @phrases = ["was to ninja for you", "threw a ninja star at your face", "is a blur", "sent you flying"].sample
+    end
+  end
+
+  class IdiotZombie < Zombie
+    def setXpPainHealth
+      @xp = 5
+      @pain = [7, 20]
+      @health = 2
+      @name = "Idiot Zombie"
+      @phrases = ["was to ninja for you", "threw a ninja star at your face", "is a blur", "sent you flying"].sample
+    end
+  end
+
+  class BlindZombie < Zombie
+    def setXpPainHealth
+      @xp = 25
+      @pain = [0, 25]
+      @health = 24
+      @name = "Blind Zombie"
+      @phrases = ["was to ninja for you", "threw a ninja star at your face", "is a blur", "sent you flying"].sample
+    end
+  end
+
+#end Stuff module
+end
