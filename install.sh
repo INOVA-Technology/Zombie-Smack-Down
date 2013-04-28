4366633af3c15730a19ec62873b33a4cde94ca0e
Addisonbean <addisonbean@gmail.com>
2013-04-28 18:04:52 -0500

    Fixed install.sh not downloading file

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSD -o /usr/local/bin/ZSD
 mkdir /usr/local/bin/ZSDFiles
-curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/game.rb -o /usr/local/bin/ZSDFiles/game.rb
-curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
+curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
+curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
 
 chmod +x /usr/local/bin/ZSD
 
