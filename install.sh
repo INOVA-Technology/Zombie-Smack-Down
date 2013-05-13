b062bfa7778cf2f94033c688bd786593e9de90bd
Addisonbean <addisonbean@gmail.com>
2013-05-13 17:36:23 -0500

    Fixed updating and installing stuff

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-sudo curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSD -o /usr/local/bin/ZSD
-sudo mkdir /usr/local/bin/ZSDFiles
-sudo curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
-sudo curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
+curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSD -o /usr/local/bin/ZSD
+mkdir /usr/local/bin/ZSDFiles
+curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
+curl -s https://raw.github.com/Addisonbean/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
 
-sudo chmod +x /usr/local/bin/ZSD
+chmod +x /usr/local/bin/ZSD
+chmod +x /usr/local/bin/ZSDFiles/game.rb
 
 echo "Success! Now enter ZSD to play Zombie Smack Down."
