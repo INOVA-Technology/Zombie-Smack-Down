7b3dec48b9735c755df214f3d20ea9f15fcbc644
dingding231 <kacmlweimer@gmail.com>
2013-05-20 15:03:51 -0500

    installer now works

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ sudo curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSD -
 sudo mkdir -p /usr/local/bin/ZSDFiles
 sudo curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
 sudo curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
+sudo curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/cost.yaml -o /usr/local/bin/ZSDFiles/cost.yaml
 
-chmod +x /usr/local/bin/ZSD
-chmod +x /usr/local/bin/ZSDFiles/game.rb
+sudo chmod +x /usr/local/bin/ZSD
+sudo chmod +x /usr/local/bin/ZSDFiles/game.rb
 
 echo "Success! Now enter ZSD to play Zombie Smack Down."
