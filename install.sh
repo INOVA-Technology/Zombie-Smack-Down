44ac80adb2c94a420000ff52fde0dd4864b67541
dingding231 <kacmlweimer@gmail.com>
2013-05-20 14:51:22 -0500

    install.sh now outputs errors

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSD -o /usr/local/bin/ZSD
+curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSD -o /usr/local/bin/ZSD
 mkdir -p /usr/local/bin/ZSDFiles
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
+curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/game.rb -o /usr/local/bin/ZSDFiles/game.rb
+curl https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/ZSDFiles/prefs.yaml -o /usr/local/bin/ZSDFiles/prefs.yaml
 
 chmod +x /usr/local/bin/ZSD
 chmod +x /usr/local/bin/ZSDFiles/game.rb
