9f0ac436dc2c9b7db4e41c458f0854c1f91bccb1
Addisonbean <addisonbean@gmail.com>
2013-05-28 19:31:14 -0500

    Added new updater

--- update.sh
+++ update.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 zsd_path="$HOME/.zsd"
-mkdir -p "$zsd_path/bin" > /dev/null
-mkdir -p "$zsd_path/require" > /dev/null
 
 curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/bin/ZSD -o "$zsd_path/bin/zsd"
 curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/game.rb -o "$zsd_path/require/game.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/prefs.yaml -o "$zsd_path/require/prefs.yaml"
 curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/update.sh -o "$zsd_path/update.sh"
 
 chmod +x "$zsd_path/bin/ZSD"
@@ -14,24 +11,13 @@ chmod +x "$zsd_path/update.sh"
 
 file1=`md5 -q $zsd_path/bin/ZSD 2> /dev/null`
 file2=`md5 -q $zsd_path/require/game.rb 2> /dev/null`
-file3=`md5 -q $zsd_path/require/prefs.yaml 2> /dev/null`
 
 git1=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/bin/ZSD | md5`
 git2=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/game.rb | md5`
-git3=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/prefs.yaml | md5`
 
-if ! cat ~/.bash_profile | grep -q "\$HOME/.zsd/bin" ; then
-	echo 'export PATH="$HOME/.zsd/bin:$PATH"' >> ~/.bash_profile
-	source ~/.bash_profile
-fi
-
-if [[ $file1 == $git1 ]] && [[ $file2 == $git2 ]] && [[ $file3 == $git3 ]]; then
+if [[ $file1 == $git1 ]] && [[ $file2 == $git2 ]]; then
 	if [[ -x "$zsd_path/bin/ZSD" ]]; then
-		if cat ~/.bash_profile | grep -q "\$HOME/.zsd/bin" ; then
-			echo -e "\033[35mSuccess! Now enter ZSD to play Zombie Smack Down. You may have to open a new window for it to work.\033[39m"
-		else
-			echo "Installation Failed. Didn't add correct path to \$PATH"
-		fi
+		echo -e "\033[35mSuccess! Now enter ZSD to play Zombie Smack Down. You may have to open a new window for it to work.\033[39m"
 	else
 		echo "Installation failed. ZSD is not executable."
 	fi
