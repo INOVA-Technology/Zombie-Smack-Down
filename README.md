bdc1eaa592939d6d3a6567b06c4fab4ad6ae6491
Addisonbean <addisonbean@gmail.com>
2014-04-10 22:07:26 -0500

    More tests!

--- README.md
+++ README.md
@@ -24,17 +24,19 @@ To-do (pull requests appreciated):
 * Add Windows & Linux support
 * Add web high scores
 * Add randomized unknown command messages
-* Read todo.txt
+* Everything else in todo.txt
+* Convert all camel case to underscores
+* Get rid of all that pInfo etc crap
 
 #iOS Support!
 Guess what. After years of waiting, popular demand has been recognized. We have finally added iOS support for Ruby 1.8.6. 
 
 
 ### IMPORTANT
-####Desktop
-* To use Zombie Smack Down, please install ruby 1.9.3 or later.
+#### Desktop
+* To use Zombie Smack Down, please install ruby 1.8.7 or later.
 
-####iOS
+#### iOS
 * The ruby installation must be 1.8.6 because 1.9.2 is broken
 * Must be logged in as root to save progress (program crashes on death)
 
--- tests.rb
+++ tests.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 $rpath = "."
 require './require/other'
 
-def exit
+def exit *args
 	"exited"
 end
 
@@ -15,14 +15,14 @@ describe Player do
 	end
 
 	describe "new game started" do
-		it "must be a new game" do 
+		it "must not longer be a new game" do 
 			@player.save[:newGame].must_equal false
 		end
 	end
 
 	describe "player is killed" do
 		before do
-			def prompt
+			def @player.prompt *args
 				"test"
 			end
 		end
@@ -32,17 +32,45 @@ describe Player do
 			@player.checkDead.must_equal "exited"
 		end
 
-		# it "must reset game" do
-			
-		# end
+		it "must reset game" do
+			expected = { :health => 25,
+						 :xp => 15,
+						 :rank => 1,
+						 :wave => 1,
+						 :zombiesKilled => 0,
+						 :totalKills => 0,
+						 :kickUpgrade => 0,
+						 :punchUpgrade => 0,
+						 :tauntsAvailable => 3,
+						 :eggUsed => false,
+						 :newGame => true }
+			@player.save[:health] = 14
+			@player.die
+			YAML.load_file("#{$rpath}/require/player.yml").must_equal expected
+		end
 
-		# it "must save score if score is a highscore" do
+		it "must save score if score is a highscore" do
+			scores_file = "#{$rpath}/scores.yml"
+			backup = YAML.load_file(scores_file)
+			@player.save[:zombiesKilled] = backup.last[0] + 1
+			@player.saveScore
+			YAML.load_file(scores_file).must_equal(
+				[[33, "Addison"], [32, "Chase"], [1, "test"],
+				[0, "nobody"], [0, "nobody"]])
+			File.open(scores_file, "w") { |file| 
+				YAML.dump(backup, file)
+			}
+			# if this test fails, the scores.yml file will have to be reset
+		end
 
-		# end
-
-		# it "wont save score if score isnt a highscore" do
-
-		# end
+		it "wont save score if score isnt a highscore" do
+			scores_file = "#{$rpath}/scores.yml"
+			backup = YAML.load_file(scores_file)
+			@player.save[:zombiesKilled] = backup.last[0] - 1
+			@player.saveScore
+			YAML.load_file(scores_file).must_equal backup
+			# if this test fails, the scores.yml file will have to be reset
+		end
 	end
 
 end
@@ -126,6 +154,70 @@ describe Cli do
 				@cli.combolist
 			end
 			assert_equal expected, message
+		end
+	end
+
+	describe "taunt" do
+		it "must require correct amount of xp" do
+
+			expected_xp = @cli.player.save[:xp] = 1
+			@cli.taunt
+			@cli.player.save[:xp].must_equal expected_xp
+			# check io
+		end
+
+		it "wont work if no taunts are left" do
+			expected_xp = @cli.player.save[:xp]
+			@cli.player.save[:tauntsAvailable] = 0
+			@cli.taunt
+			@cli.player.save[:xp].must_equal expected_xp
+			# check io
+		end
+	end
+
+	describe "info" do
+		it "must print correct player info" do
+			new_save = { :health => 48,
+						 :xp => 27,
+						 :rank => 2,
+						 :wave => 2,
+						 :zombiesKilled => 6,
+						 :totalKills => 37,
+						 :kickUpgrade => 4,
+						 :punchUpgrade => 3,
+						 :tauntsAvailable => 2 }
+			@cli.player.save.merge! new_save
+
+			actual = capture_io do
+				@cli.player.info
+			end
+
+			expected = capture_io do
+				puts "Health: 48".magenta
+				puts "XP: 27".magenta
+				puts "Rank: 2".magenta
+				puts "Wave: 2".magenta
+				puts "Zombies Killed: 6".magenta
+				puts "Total Kills: 37".magenta
+				puts "Kick Upgrade: 4".magenta
+				puts "Punch Upgrade: 3".magenta
+				puts "Taunts Available: 2".magenta
+			end
+			assert_equal expected, actual
+		end
+
+		it "must print correct zombie info" do
+			@cli.zombie = DaddyZombie.new
+			@cli.zombie.health = 2
+			actual = capture_io do
+				@cli.zombie.info
+			end
+
+			expected = capture_io do
+				puts "Daddy Zombie health: 2".magenta
+				puts "Attack Strength: 4 to 10".magenta
+			end
+			assert_equal expected, actual
 		end
 	end
 
