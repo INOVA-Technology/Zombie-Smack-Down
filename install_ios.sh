e024483360ea3722d7398984803d80c18c61e0ab
Chase <kacmlweimer@gmail.com>
2014-04-12 11:55:36 -0500

    doesnt work yet but wut evs

--- install_ios.sh
+++ install_ios.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
-zsd_path="$HOME/.zsd"
-mkdir -p "$zsd_path" > /dev/null
-mkdir -p "$zsd_path/require" > /dev/null
+if [ ! $(which curl) ]; then
+    echo "ERROR: Please install the curl command, then try again."
+    exit
+fi
 
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/zsd -o "$zsd_path/zsd"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/colors.rb -o "$zsd_path/require/colors.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/combo.rb -o "$zsd_path/require/combo.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/other.rb -o "$zsd_path/require/other.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.rb -o "$zsd_path/require/player.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/zombie.rb -o "$zsd_path/require/zombie.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.yml -o "$zsd_path/require/player.yml"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/scores.yml -o "$zsd_path/scores.yml"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/update.sh -o "$zsd_path/update.sh"
 
-chmod +x "$zsd_path/zsd"
-chmod +x "$zsd_path/update.sh"
+mkdir -p "usr/bin/zsd"
+mkdir -p "usr/bin/zsd/require"
+
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/zsd -o "usr/bin/zsd/zsd"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/colors.rb -o "usr/bin/zsd/require/colors.rb"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/combo.rb -o "usr/bin/zsd/require/combo.rb"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/other.rb -o "usr/bin/zsd/require/other.rb"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.rb -o "usr/bin/zsd/require/player.rb"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/zombie.rb -o "usr/bin/zsd/require/zombie.rb"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.yml -o "usr/bin/zsd/require/player.yml"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/scores.yml -o "usr/bin/zsd/scores.yml"
+curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/update.sh -o "usr/bin/zsd/update.sh"
+
+chmod +x "usr/bin/zsd/zsd"
+chmod +x "usr/bin/zsd/update.sh"
 
 
 
 if ! cat ~/.bash_profile | grep -q "\$HOME/.zsd" ; then
-	echo 'export PATH="$HOME/.zsd:$PATH"' >> ~/.bash_profile
+	echo 'export PATH="/usr/bin/zsd:$PATH"' >> ~/.bash_profile
 	source ~/.bash_profile
 fi
 
 
-if [[ -x "$zsd_path/zsd" ]]; then
+if [[ -x "usr/bin/zsd/zsd" ]]; then
 	if cat ~/.bash_profile | grep -q "\$HOME/.zsd" ; then
 		 curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/man/zsd.6 -o "/usr/share/man/man6/zsd.6"
 		echo -e "\033[35mSuccess! Now enter zsd to play Zombie Smack Down. You may have to open a new window for it to work.\033[39m"
