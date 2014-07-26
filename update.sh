c485360e3bd2179cf45f77c92e7cf169e6ce1623
addisonbean <addisonbean@gmail.com>
2014-07-25 19:45:33 -0500

    Fixed installer

--- update.sh
+++ update.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
 
-zsd_path="$HOME/.zsd"
+ZSD_PATH="$HOME/.zsd"
 
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/zsd -o "$zsd_path/zsd"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/colors.rb -o "$zsd_path/require/colors.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/combo.rb -o "$zsd_path/require/combo.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/other.rb -o "$zsd_path/require/other.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.rb -o "$zsd_path/require/player.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/zombie.rb -o "$zsd_path/require/zombie.rb"
-curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/update.sh -o "$zsd_path/update.sh"
-
-chmod +x "$zsd_path/zsd"
-chmod +x "$zsd_path/update.sh"
-
-file1=`md5 -q $zsd_path/zsd 2> /dev/null`
-file2=`md5 -q $zsd_path/require/colors.rb 2> /dev/null`
-file3=`md5 -q $zsd_path/require/combo.rb 2> /dev/null`
-file4=`md5 -q $zsd_path/require/other.rb 2> /dev/null`
-file5=`md5 -q $zsd_path/require/player.rb 2> /dev/null`
-file6=`md5 -q $zsd_path/require/zombie.rb 2> /dev/null`
-
-git1=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/zsd | md5`
-git2=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/colors.rb | md5`
-git3=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/combo.rb | md5`
-git4=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/other.rb | md5`
-git5=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/player.rb | md5`
-git6=`curl -s https://raw.github.com/INOVA-Technology/Zombie-Smack-Down/master/require/zombie.rb | md5`
-
-if [[ $file1 == $git1 ]] && [[ $file2 == $git2 ]] && [[ $file3 == $git3 ]] && [[ $file4 == $git4 ]] && [[ $file5 == $git5 ]] && [[ $file6 == $git6 ]]; then
-	if [[ -x "$zsd_path/zsd" ]]; then
-		echo -e "\033[35mUpdate successful!\033[39m"
-	else
-		echo "Installation failed. ZSD is not executable."
+download_file() {
+	from_url="https://raw.githubusercontent.com/INOVA-Technology/Zombie-Smack-Down/master/$1"
+	curl -s $from_url -o "$ZSD_PATH/$1"
+	git=$(md5 -q "$ZSD_PATH/$1" 2> /dev/null)
+	file=$(curl -s https://raw.githubusercontent.com/INOVA-Technology/Zombie-Smack-Down/master/$1 | md5)
+	if [[ $git == $file ]]; then
+		echo "all good"
 	fi
+}
+
+status1=$(download_file zsd)
+status2=$(download_file require/colors.rb)
+status3=$(download_file require/combo.rb)
+status4=$(download_file require/other.rb)
+status5=$(download_file require/player.rb)
+status6=$(download_file require/zombie.rb)
+status7=$(download_file update.sh)
+
+if [[ $status1 ]] && [[ $status2 ]] && [[ $status3 ]] && [[ $status4 ]] && [[ $status5 ]] && [[ $status6 ]] && [[ $status7 ]]; then
+	echo -e "\033[35mSuccess!\033[39m"
 else
-	echo "Update failed. Files were not downloaded (correctly)."
+	echo "Installation failed. Files were not downloaded (correctly)."
 fi
